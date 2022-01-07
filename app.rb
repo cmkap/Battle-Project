561ec47509d5989fed2f4edf0dc4ceb41ed05521
chisambwe <c.m.kapihya@gmail.com>
2022-01-07 15:09:57 +0000

    Entering Players

--- app.rb
+++ app.rb
@@ -6,8 +6,20 @@ class Battle_Project < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
+  get '/test' do
     'Testing infrastucture working!'
+  
+  end
+
+  get '/' do
+  
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
---
+++ spec/features/testing_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Testing names' do
+    scenario 'submitting names' do
+        visit('/')
+        fill_in :player_1_name, with: 'Charlotte'
+        fill_in :player_2_name, with: 'Bruno'
+        click_button 'Submit'
+        expect(page).to have_content 'Charlotte vs. Bruno'
+    end
+end
