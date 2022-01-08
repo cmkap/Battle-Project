86643f47e00cb3cae443e467fafc5fb1d9f47091
chisambwe <c.m.kapihya@gmail.com>
2022-01-08 16:56:25 +0000

    add test helpers

--- spec/features/testing_hit_points_spec.rb
+++ spec/features/testing_hit_points_spec.rb
@@ -1,8 +1,6 @@
 feature 'to see Players hit points' do
     scenario 'see hit points' do
-        visit('/')
-        fill_in :player_1_name, with: 'Tim'
-        fill_in :player_2_name, with: 'Bryan'
-        click_button 'Submit'
-        expect(page).to have_content 'Bryan: 70HP'
+       sign_in_and_play
+        expect(page).to have_content 'Bruno: 70HP'
     end
+end
--- spec/features/testing_names_spec.rb
+++ spec/features/testing_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Testing names' do
     scenario 'submitting names' do
-        visit('/')
-        fill_in :player_1_name, with: 'Charlotte'
-        fill_in :player_2_name, with: 'Bruno'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Charlotte vs. Bruno'
     end
 end
