6e71e188e3cadc0ff6c2295beaa888e3b8b64f52
chisambwe <c.m.kapihya@gmail.com>
2022-01-09 09:24:50 +0000

    App class name change & logic extraction

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-class Battle_Project < Sinatra::Base
+class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
@@ -19,22 +19,22 @@ class Battle_Project < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
     
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
